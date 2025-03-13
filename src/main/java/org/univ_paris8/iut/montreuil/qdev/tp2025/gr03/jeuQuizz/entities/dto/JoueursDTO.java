4b4226cc4a0fbca1f56bb24963f0b0d4a37c9f18
MangaDMFZ <mdiabate@iut.univ-paris8.fr>
2025-03-13 11:51:44 +0100

    ajout constructeur JoueurDTO

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr03/jeuQuizz/entities/dto/JoueursDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr03/jeuQuizz/entities/dto/JoueursDTO.java
@@ -1,4 +1,21 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr03.jeuQuizz.entities.dto;
 
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr03.jeuQuizz.utils.objet.Langue;
+
 public class JoueursDTO {
+    private String pseudo;
+    private Langue langue;
+    private  String prenom;
+    private int annee_naissance;
+    private String interets;
+
+
+
+    public JoueursDTO(String pseudo, Langue langue, String interets, String prenom , int annee_naissance){
+        this.pseudo=pseudo;
+        this.langue=langue;
+        this.interets=interets;
+        this.prenom=prenom;
+        this.annee_naissance=annee_naissance;
+    }
 }
